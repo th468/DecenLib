d999d4516b6f7905dd875bb02a6cf5f3431b1296
tk468 <tkhasegawa.ti@kaien-t2.net>
2026-08-10 14:29:20 +0900

    fix: ALLOWED_HOSTを環境変数を用いて変更できるよう編集

--- library/config/settings.py
+++ library/config/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS",
+    default=[".onrender.com", "localhost", "127.0.0.1"],
+)
 
 
 # Application definition
